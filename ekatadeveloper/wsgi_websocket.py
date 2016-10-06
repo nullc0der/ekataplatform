037296dfc9a24acbbab1a7176020a05ebab5215e
c0defreak <prasantakakati1994@gmail.com>
2016-10-06 22:39:48 +0530

    fixed typo

--- ekatadeveloper/wsgi_websocket.py
+++ ekatadeveloper/wsgi_websocket.py
@@ -1,13 +1,6 @@
-import os
-import gevent
-import redis.connection
-
-redis.connection.socket = gevent.socket
-
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "ekatadeveloper.settings.production"
-)
+# entry point for the websocket loop
+import gevent.monkey
+gevent.monkey.patch_thread()
 
 from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
-
 application = uWSGIWebsocketServer()
