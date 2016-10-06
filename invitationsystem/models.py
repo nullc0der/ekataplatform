9330dae756a3bec6157952e9481aa82049e1af81
c0defreak <prasantakakati1994@gmail.com>
2016-10-07 04:51:44 +0530

    added default to invitation id

--- invitationsystem/models.py
+++ invitationsystem/models.py
@@ -1,15 +1,24 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.crypto import get_random_string
 
 from invitationsystem.tasks import send_invitation
 
 # Create your models here.
 
 
+def random_string():
+    return get_random_string(length=6)
+
+
 class Invitation(models.Model):
     email = models.CharField(max_length=100)
-    invitation_id = models.CharField(max_length=300)
+    invitation_id = models.CharField(
+        max_length=300,
+        default=random_string,
+        editable=False
+    )
     approved = models.BooleanField(default=False)
     sent = models.BooleanField(default=False, editable=False)
 
