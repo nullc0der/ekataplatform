f1af729dcc38c8c4b6a12a3aac9d68e4bc5ba24a
c0defreak <prasantakakati1994@gmail.com>
2016-11-26 17:00:05 +0530

    EP-257:Change URL navigation

--- groupsystem/urls.py
+++ groupsystem/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
         name='groupdetails'
     ),
     url(
-        r'^(?P<group_id>\d+)/n/(?P<news_id>\d+)/$',
+        r'^group/(?P<group_id>\d+)/n/(?P<news_id>\d+)/$',
         views.news_details,
         name='newsdetails'
     ),
@@ -54,32 +54,32 @@ urlpatterns = [
         name='postdetail'
     ),
     url(
-        r'^(?P<group_id>\d+)/l/(?P<post_id>\d+)/$',
+        r'^group/(?P<group_id>\d+)/l/(?P<post_id>\d+)/$',
         views.like_post,
         name='like'
     ),
     url(
-        r'^c/(?P<group_id>\d+)/(?P<post_id>\d+)/$',
+        r'^group/c/(?P<group_id>\d+)/(?P<post_id>\d+)/$',
         views.comment_post,
         name='comment'
     ),
     url(
-        r'^creatememberpost/(?P<group_id>\d+)/$',
+        r'^group/creatememberpost/(?P<group_id>\d+)/$',
         views.create_member_post,
         name='creatememberpost'
     ),
     url(
-        r'^(?P<group_id>\d+)/admin/settings/$',
+        r'^group/(?P<group_id>\d+)/admin/settings/$',
         views.group_admin_settings_page,
         name='adminsettings'
     ),
     url(
-        r'^(?P<group_id>\d+)/admin/news/$',
+        r'^group/(?P<group_id>\d+)/admin/news/$',
         views.group_admin_news_page,
         name='adminnews'
     ),
     url(
-        r'^(?P<group_id>\d+)/editnews/$',
+        r'^group/(?P<group_id>\d+)/editnews/$',
         views.edit_news,
         name='editnews'
     ),
@@ -114,77 +114,77 @@ urlpatterns = [
         name='approvecomment'
     ),
     url(
-        r'^notapprovedcomments/(?P<group_id>\d+)/$',
+        r'^group/notapprovedcomments/(?P<group_id>\d+)/$',
         views.notapproved_comment_admin_page,
         name='notapprovedcomments'
     ),
     url(
-        r'^approvedcomments/(?P<group_id>\d+)/$',
+        r'^group/approvedcomments/(?P<group_id>\d+)/$',
         views.approved_comment_admin_page,
         name='approvedcomments'
     ),
     url(
-        r'^editcomment/(?P<group_id>\d+)/(?P<comment_id>\d+)/$',
+        r'^group/editcomment/(?P<group_id>\d+)/(?P<comment_id>\d+)/$',
         views.edit_comment_admin_page,
         name='editcomment'
     ),
     url(
-        r'^joinrequests/(?P<group_id>\d+)/$',
+        r'^group/joinrequests/(?P<group_id>\d+)/$',
         views.joinrequest_admin_page,
         name='joinrequest'
     ),
     url(
-        r'^members/(?P<group_id>\d+)/$',
+        r'^group/members/(?P<group_id>\d+)/$',
         views.group_member_page,
         name='members'
     ),
     url(
-        r'^bannedmembers/(?P<group_id>\d+)/$',
+        r'^group/bannedmembers/(?P<group_id>\d+)/$',
         views.group_banned_members_page,
         name='bannedmembers'
     ),
     url(
-        r'^userautocomplete/(?P<group_id>\d+)/$',
+        r'^group/userautocomplete/(?P<group_id>\d+)/$',
         views.users_autocomplete,
         name='users_autocomplete'
     ),
     url(
-        r'^inviteuser/(?P<group_id>\d+)/$',
+        r'^group/inviteuser/(?P<group_id>\d+)/$',
         views.invite_user,
         name='inviteuser'
     ),
     url(
-        r'^(?P<group_id>\d+)/admin/events/$',
+        r'^group/(?P<group_id>\d+)/admin/events/$',
         views.group_events_admin_page,
         name='eventsadmin'
     ),
     url(
-        r'^createnotification/(?P<group_id>\d+)/$',
+        r'^group/createnotification/(?P<group_id>\d+)/$',
         views.create_notification_admin,
         name='createnotification'
     ),
     url(
-        r'^(?P<group_id>\d+)/admin/dashboard/$',
+        r'^group/(?P<group_id>\d+)/admin/dashboard/$',
         views.group_dashboard_admin_page,
         name='groupdashboard'
     ),
     url(
-        r'^(?P<group_id>\d+)/admin/defaultroles/$',
+        r'^group/(?P<group_id>\d+)/admin/defaultroles/$',
         views.group_default_role_admin_page,
         name='groupdefaultrole'
     ),
     url(
-        r'^(?P<group_id>\d+)/admin/customroles/$',
+        r'^group/(?P<group_id>\d+)/admin/customroles/$',
         views.group_custom_role_admin_page,
         name='groupcustomrole'
     ),
     url(
-        r'^editroleperm/(?P<group_id>\d+)/(?P<custom_role_id>\d+)/$',
+        r'^group/editroleperm/(?P<group_id>\d+)/(?P<custom_role_id>\d+)/$',
         views.edit_permissions,
         name='editperm'
     ),
     url(
-        r'^edituserperm/(?P<group_id>\d+)/(?P<user_id>\d+)/$',
+        r'^group/edituserperm/(?P<group_id>\d+)/(?P<user_id>\d+)/$',
         views.edit_extra_permissions,
         name='edituserperm'
     ),
