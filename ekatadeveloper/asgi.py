4987605c186c707f8b487e64ee9cce59081cde62
c0defreak <prasantakakati1994@gmail.com>
2017-01-07 21:28:28 +0530

    removed sentry from asgi

--- ekatadeveloper/asgi.py
+++ ekatadeveloper/asgi.py
@@ -1,8 +1,6 @@
 import os
 from channels.asgi import get_channel_layer
 
-from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ekatadeveloper.settings")
 
-channel_layer = Sentry(get_channel_layer())
+channel_layer = get_channel_layer()
