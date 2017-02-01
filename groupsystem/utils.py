486d471a7f534aa7aeb7e49f22953b779566c72d
Prasanta Kakati <prasantakakati1994@gmail.com>
2017-02-02 03:26:11 +0530

    added real url

--- groupsystem/utils.py
+++ groupsystem/utils.py
@@ -26,6 +26,6 @@ def send_serialized_user(pk_set):
     users = User.objects.filter(id__in=pk_set)
     data = serializers.serialize('json', users)
     payload = {'data': data}
-    url = 'http://localhost:8001/en/type/integrate_users/'
+    url = 'https://development.ekata.social/en/type/integrate_users/'
     res = requests.post(url, data=payload)
     return res.status_code
