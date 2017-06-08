637df1a3344c3b8a049ab33518e6c47cee6efdc9
c0defreak <prasantakakati1994@gmail.com>
2017-06-08 23:45:18 +0530

    changed some email conf

--- ekatadeveloper/settings/development.py
+++ ekatadeveloper/settings/development.py
@@ -27,5 +27,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = get_env_variable('DJANGO_EMAIL_HOST')
 EMAIL_HOST_USER = get_env_variable('DJANGO_EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = get_env_variable('DJANGO_EMAIL_HOST_PASSWORD')
-EMAIL_PORT = 2525
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
